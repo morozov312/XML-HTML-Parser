2fb97aaa272575b98e50cf3a52a85a1d76b99406
Morozov312 <alexander.morozov312@gmail.com>
2021-01-14 01:23:49 +0700

    add library for smart select

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
 'use strict'
+import * as select from './select'
---
+++ src/js/select.js
@@ -0,0 +1,6 @@
+export default new SlimSelect({
+    select: '#faculty',
+    placeholder: 'Choose a faculty',
+    showSearch: false,
+    hideSelectedOption: true
+})
